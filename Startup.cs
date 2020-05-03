5004a091fede0c29e8defeef2b39025f3b6aefd5
dhananshan <navin.dhananshan@outlook.com>
2020-05-03 19:14:36 +0800

    removed UseHsts

--- Startup.cs
+++ Startup.cs
@@ -43,7 +43,7 @@ namespace AllCommands
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
             app.UseOutputCaching();
